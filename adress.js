e8da8e805c045f59189aa7437ca230eed3e70735
stephanie nyangate <stephanienyangate@gmail.com>
2026-05-12 17:48:56 +0300

    feat(js):give contacts id

--- adress.js
+++ adress.js
@@ -1,4 +1,11 @@
 // BUSINESS LOGIC
+// start with the name and pnone numbers
+function Contact(first, last, phone){
+    this.firstName= first;
+    this.lastName= last;
+    this.phoneNumber= phone;
+}
+
 function AdressBook() {
     this.contaccts = {};
     this.currentId = 0;
@@ -6,5 +13,10 @@ function AdressBook() {
 
 AdressBook.prototype.assignId = function() {
     this.currentId= this.currentId + 1;
-    
+    return this.currentId;
+}
+
+AdressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contact[contact.id]= contact;
 }
