2c4060ec7ef36857ec5e2512765e8ef58dae057a
stephanie nyangate <stephanienyangate@gmail.com>
2026-05-12 23:21:24 +0300

    feat(JS):add prototype for adding the task and deleting the task

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function ToDoList() {
     this.tasks = {};
     this.currentId = 0;
 }
+
+ToDoList.prototype.assignId = function () {
+    this.currentId += 1;
+    return this.currentId;
+};
+
+ToDoList.prototype.addTask = function (task) {
+    task.id = this.assignId();
+    this.tasks[task.id] = task;
+};
+
+ToDoList.prototype.deleteTask = function (id) {
+    if (this.tasks[id] === undefined) {
+        return false;
+    }
+    delete this.tasks[id];
+    return true;
+};
