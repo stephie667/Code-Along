91e9818d24f772edef11eeff3425ae6341886730
stephanie nyangate <stephanienyangate@gmail.com>
2026-05-12 20:42:52 +0300

    feat(js):add function to update the list on the website

--- adress.js
+++ adress.js
@@ -20,3 +20,22 @@ AdressBook.prototype.addContact = function(contact) {
     contact.id = this.assignId();
     this.contact[contact.id]= contact;
 }
+
+// User Interface
+let myAdressBook= new AdressBook();
+function showContacts(book) {
+    let listArea =$("#display-lists");
+    let htmlString= "";
+
+    Object.keys(book.contacts).forEach(function(id) {
+        let person=book.contacts[id];
+        htmlString += "<li>" + 
+        person.firstName +" "+
+        person.lastName + "-" +
+        person.phoneNumber + "</li>";
+
+    });
+    listArea.html(htmlString);
+}
+
+$
